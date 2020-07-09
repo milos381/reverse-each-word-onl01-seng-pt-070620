0a2c90c1d3b343f37a8e357508ba67f1a3e402d3
Milos Popovic <milos_popovic@outlook.com>
2020-07-09 21:56:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   my_new = []
-  splitword = string.split(" ")
+  splitword = string.split(" ") #we need to make it into an array. Each doesn't work with strings
   splitword.each do |words|
-    my_new << words.reverse
+    my_new << words.reverse #creating new array
   end
-  my_new.join(" ")
+  my_new.join(" ") #array to string
 end
